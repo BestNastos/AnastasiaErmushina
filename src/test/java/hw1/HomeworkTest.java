512ba23e35a995c609d5c82e4e9a8dd746faa2f9
bestnastos <bestnastos@gmail.com>
2019-01-25 15:16:13 +0300

    Initial commit

---
+++ src/test/java/hw1/HomeworkTest.java
@@ -0,0 +1,123 @@
+package hw1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+import static java.lang.System.setProperty;
+import static org.testng.Assert.assertTrue;
+
+public class HomeworkTest {
+
+//    @BeforeClass
+//    public void beforeClass(){
+//        setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+//    }
+
+    @Test
+    public void openSite() {
+
+        //1 Open test site by URL
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
+
+        //2 Assert Browser title
+
+        assertEquals(driver.getTitle(), "Home Page");
+
+        //3 Perform login
+
+        driver.findElement(By.cssSelector("[id='user-icon']")).click();
+        driver.findElement(By.cssSelector("[id='name']")).sendKeys("epam");
+        driver.findElement(By.cssSelector("[id='password']")).sendKeys("1234");
+        driver.findElement(By.cssSelector("[id='login-button']")).click();
+
+        //4 Assert User name in the left-top side of screen that user is loggined
+
+        WebElement userName = driver.findElement(By.cssSelector("[id='user-name']"));
+        assertEquals(userName.getText(), "PITER CHAILOVSKII");
+
+        //5 Assert Browser title
+
+        assertEquals(driver.getTitle(), "Home Page");
+
+        //6 Assert that there are 4 items on the header section are displayed and they have proper texts
+        List<WebElement> navigationBar = driver.findElements(By.cssSelector("header > div > nav > ul"));
+        List<WebElement> elements = navigationBar.get(0).findElements(By.xpath("*"));
+        assertEquals(elements.get(0).getText(), "HOME");
+        assertEquals(elements.get(1).getText(), "CONTACT FORM");
+        assertEquals(elements.get(2).getText(), "SERVICE");
+        assertEquals(elements.get(3).getText(), "METALS & COLORS");
+
+
+        //7 Assert that there are 4 images on the Index Page and they are displayed
+//        List <WebElement> images = driver.findElements(By.tagName("img"));
+//        assertEquals(images.size(), 4);
+
+        //8 Assert that there are 4 texts on the Index Page under icons and they have proper text
+        List<WebElement> textUnderIcons = driver.findElements(By.className("benefit-txt"));
+        assertEquals(textUnderIcons.get(0).getText(), "To include good practices\n" +
+                "and ideas from successful\n" +
+                "EPAM project");
+        assertEquals(textUnderIcons.get(1).getText(), "To be flexible and\n" +
+                "customizable");
+        assertEquals(textUnderIcons.get(2).getText(), "To be multiplatform");
+        assertEquals(textUnderIcons.get(3).getText(), "Already have good base\n" +
+                "(about 20 internal and\n" +
+                "some external projects),\n" +
+                "wish to get more…");
+
+        //9 Assert a text of the main headers
+        WebElement mainHeader1 = driver.findElement(By.cssSelector("body > div > div.uui-main-container > main > div.main-content > h3.main-title.text-center"));
+        assertEquals(mainHeader1.getText(), "EPAM FRAMEWORK WISHES…");
+        WebElement mainHeader2 = driver.findElement(By.cssSelector("body > div > div.uui-main-container > main > div.main-content > p"));
+        assertEquals(mainHeader2.getText(), "LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISICING ELIT, SED DO EIUSMOD TEMPOR INCIDIDUNT UT LABORE ET DOLORE MAGNA ALIQUA. UT ENIM AD MINIM VENIAM, QUIS NOSTRUD EXERCITATION ULLAMCO LABORIS NISI UT ALIQUIP EX EA COMMODO CONSEQUAT DUIS AUTE IRURE DOLOR IN REPREHENDERIT IN VOLUPTATE VELIT ESSE CILLUM DOLORE EU FUGIAT NULLA PARIATUR.");
+
+        //10 Assert that there is the iframe in the center of page
+        List<WebElement> iframes = driver.findElements(By.tagName("iframe"));
+        assertTrue(iframes.size() > 0);
+
+        //11
+        WebDriver frame = driver.switchTo().frame(iframes.get(0));
+        boolean isLogoFound = false;
+        try {
+            frame.findElement(By.cssSelector("#epam_logo"));
+            isLogoFound = true;
+        } catch (Exception e) {
+            isLogoFound = false;
+        }
+        assertTrue(isLogoFound);
+
+        //12 Switch to original window back
+        frame.switchTo().defaultContent();
+        // did it actually switch? TODO
+
+        //13 Assert a text of the sub header
+        WebElement subHeader = frame.findElement(By.cssSelector("body > div > div.uui-main-container > main > div.main-content > h3:nth-child(3) > a"));
+        assertEquals(subHeader.getText(), "JDI GITHUB");
+
+        //14 Assert that JDI GITHUB is a link and has a proper URL
+        assertEquals(subHeader.getAttribute("href"), "https://github.com/epam/JDI");
+
+        //15 Assert that there is Left Section
+        WebElement leftSection = frame.findElement(By.cssSelector("#mCSB_1"));
+        assertTrue(leftSection.isDisplayed());
+
+        //16 Assert that there is Footer
+        WebElement footer = frame.findElement(By.cssSelector("footer > div > div"));
+        assertTrue(footer.isDisplayed());
+
+        // 17 Close Browser
+        driver.close();
+    }
+}
