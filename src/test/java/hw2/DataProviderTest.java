74a60f0acb7361c98f59e40b8f2d91b28e5ddf78
bestnastos <bestnastos@gmail.com>
2019-01-27 17:02:29 +0300

    hw2

--- src/test/java/hw2/DataProviderTest.java
+++ src/test/java/hw2/DataProviderTest.java
@@ -1,14 +1,41 @@
 package hw2;
 
-import org.testng.annotations.DataProvider;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
 
 public class DataProviderTest {
 
-//    @DataProvider(parallel = true)
-//    public Object[][] dataProvider(){
-//
-//    }
+    @DataProvider(parallel = true)
+    public Object[][] dataProvider() {
+        return new Object[][]{
+                {"practise", "To include good practices\n" +
+                        "and ideas from successful\n" +
+                        "EPAM project"},
+                {"custom", "To be flexible and\n" +
+                        "customizable"},
+                {"multi", "To be multiplatform"},
+                {"base", "Already have good base\n" +
+                        "(about 20 internal and\n" +
+                        "some external projects),\n" +
+                        "wish to get more…"}
+        };
+    }
 
+    @Test(dataProvider = "dataProvider", threadPoolSize = 4)
+    public void iconTextTest(String selector, String expectedText) {
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://epam.github.io/JDI/index.html");
 
+        WebElement actualText = driver.findElement(By.className("icon-" + selector))
+                .findElement(By.xpath("../.."))
+                .findElement(By.cssSelector("[class='benefit-txt']"));
+        assertEquals(actualText.getText(), expectedText);
 
+        driver.close();
+    }
 }
