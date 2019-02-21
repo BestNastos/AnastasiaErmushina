cf719ee7c6b1778b5e9ce37281bf60a6e538b6b4
bestnastos <bestnastos@gmail.com>
2019-02-22 01:42:40 +0300

    metals and colors page

--- src/main/java/Homework/hw7/pages/MetalsAndColorsPage.java
+++ src/main/java/Homework/hw7/pages/MetalsAndColorsPage.java
@@ -1,10 +1,29 @@
 package Homework.hw7.pages;
 
+import com.epam.jdi.light.elements.complex.Droplist;
 import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
+import com.epam.jdi.light.elements.pageobjects.annotations.objects.JDropdown;
 import com.epam.jdi.light.elements.pageobjects.annotations.simple.Css;
+import com.epam.jdi.light.ui.html.complex.Checklist;
+import com.epam.jdi.light.ui.html.complex.RadioButtons;
 
 public class MetalsAndColorsPage extends WebPage {
+
+    @Css("[id='odds-selector']")
+    public static RadioButtons oddNumbersSummary;
+
+    @Css("[id='even-selector']")
+    public static RadioButtons evenNumbersSummary;
+
+    @Css("[id='elements-checklist']")
+    public static Checklist forcesOfNature;
+
+    @JDropdown(root = "div[ui=dropdown]",
+            value = ".filter-option",
+            list = "li",
+            expand = ".caret")
+    public static Droplist colors;
+
 
 
 
