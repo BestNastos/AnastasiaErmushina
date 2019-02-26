4d1b215e9839b0b0eef1dec2cf8557187b01530b
bestnastos <bestnastos@gmail.com>
2019-02-27 02:18:43 +0300

    classwork for 25.02

--- src/test/java/JDI/ExampleJDI.java
+++ src/test/java/JDI/ExampleJDI.java
@@ -1,7 +1,10 @@
 package JDI;
 
+import com.epam.jdi.light.actions.ActionHelper;
 import com.epam.jdi.light.driver.WebDriverFactory;
+import com.epam.jdi.light.settings.WebSettings;
 import com.epam.jdi.light.ui.html.PageFactory;
+import cucumber.api.java.After;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -11,6 +14,11 @@ public class ExampleJDI {
     @BeforeSuite
     public void beforeSuite() {
         PageFactory.initElements(EpamSite.class);
+//        WebSettings.SEARCH_CONDITION = e -> {return e.isDisplayed();};
+//        ActionHelper.BEFORE_JDI_ACTION = jp ->{};
+//        ActionHelper.AFTER_JDI_ACTION = (jp, o) ->{
+//            return ActionHelper.AFTER_STEP_ACTION;
+//        }; // check if correct (teacher's repository)
 
     }
 
