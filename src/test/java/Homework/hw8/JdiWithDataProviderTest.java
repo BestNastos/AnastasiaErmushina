69de8fef5dde9ea2d3333c599144eb08bde59938
bestnastos <bestnastos@gmail.com>
2019-03-04 11:55:32 +0300

    hw 8 data provider fixed

--- src/test/java/Homework/hw8/JdiWithDataProviderTest.java
+++ src/test/java/Homework/hw8/JdiWithDataProviderTest.java
@@ -4,8 +4,8 @@ import Homework.hw8.pages.EpamWebsite;
 import Homework.hw8.utils.MetalsColorsFormData;
 import com.epam.jdi.light.driver.WebDriverFactory;
 import com.epam.jdi.light.ui.html.PageFactory;
-import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.testng.annotations.AfterMethod;
@@ -15,6 +15,7 @@ import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Map;
 
 import static Homework.hw8.pages.EpamWebsite.homePage;
 import static Homework.hw8.pages.EpamWebsite.metalsAndColorsPage;
@@ -51,14 +52,14 @@ public class JdiWithDataProviderTest {
 
     @DataProvider
     public Object[][] provideData() throws FileNotFoundException {
-        JsonObject jsonObject = new JsonParser()
+        JsonObject jsonMap = new JsonParser()
                 .parse(new FileReader("src/test/resources/JDI_ex8_metalsColorsDataSet.json"))
                 .getAsJsonObject();
-        Object[][] returnedArray = new Object[jsonObject.size()][1];
-        for (int i = 0; i < returnedArray.length; i++) {
-            returnedArray[i][0] = new Gson()
-                    .fromJson(jsonObject.get("data_" + (i + 1)), new TypeToken<MetalsColorsFormData>() {
-                    }.getType());
+        Object[][] returnedArray = new Object[jsonMap.size()][1];
+        int i= 0;
+        for (Map.Entry<String, JsonElement> entry : jsonMap.entrySet()) {
+            returnedArray[i++][0] = new Gson()
+                    .fromJson(entry.getValue(), MetalsColorsFormData.class);
         }
         return returnedArray;
     }
